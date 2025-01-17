015e24d607bc5fb2a7b933dc178f45bf5fb19a37
lamb-qwey <lambqwey@gmail.com>
2025-01-16 18:34:48 -0600

    working: normalization

---
+++ dynamic-world-norm.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+
+import os
+import warnings
+import numpy as np
+import rasterio
+import argparse
+import matplotlib.pyplot as plt
+plt.ioff() # ensure interactive matplotlib is deactivated
+
+
+# CALL:
+# python3 dynamic-world-norm.py /bulk-1/rasters/2023-07-04_index_blue_test-buffer_filled_aligned.tif /bulk-1/rasters/2023-07-04_index_blue_test-buffer_filled_aligned_normalized.tif -p
+
+# normalize single band geotiff with dynamic world method
+    # 1. Log transform to handle long-tailed values
+    # 2. Calculate 1st and 99th percentiles of log-transformed data
+    # 3. Remap these percentiles to sigmoid curve
+    # results in reshaped DN, with extremes squished but not clipped, bounded on (0, 1)
+
+def plot_transformations(data_dict, output_path):
+    """
+A 2x2 grid showing the raster at each stage of transformation.
+    """
+    fig, axs = plt.subplots(2, 2, figsize=(15, 15))
+    stages = ['original', 'log_transformed', 'scaled', 'normalized']
+
+    for i, (stage, data) in enumerate(data_dict.items()):
+        ax = axs[i//2, i%2]
+        im = ax.imshow(data, cmap='viridis')
+        ax.set_title(stage.capitalize())
+        plt.colorbar(im, ax=ax)
+
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
+
+def plot_histograms(data_dict, output_path):
+    """
+A 2x2 grid showing histograms of the data at each stage of transformation.
+    """
+    fig, axs = plt.subplots(2, 2, figsize=(15, 15))
+    stages = ['original', 'log_transformed', 'scaled', 'normalized']
+
+    for i, (stage, data) in enumerate(data_dict.items()):
+        ax = axs[i//2, i%2]
+        ax.hist(data.ravel(), bins=50)
+        ax.set_title(f'{stage.capitalize()} Histogram')
+        ax.set_xlabel('Value')
+        ax.set_ylabel('Frequency')
+
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
+
+def counts(data, name, nodata_value):
+    """
+    Check data set properties for diagnostics
+    """
+
+    #arg properties
+    print("ARG PROPERTIES:")
+    print(f"name: {name}")
+    print(f"ndval: {nodata_value}")
+    print(f"data type: {data.dtype}")
+    print(f"Data range: {data.min()} to {data.max()}")
+
+    #Nan and masked
+    print("NAN AND MASKED")
+    if np.ma.is_masked(data):
+        print(f"Number of masked values: {data.mask.sum()}")
+    print(f"n_nan: {np.count_nonzero(np.isnan(data))}")
+
+    #nodata value
+    print("NODATA")
+    print(f"n_neg10k_f: {np.count_nonzero(data == -10000.0)}")
+    print(f"n_neg10k_i: {np.count_nonzero(data == -10000)}")
+    print(f"n_neg10k_close: {np.count_nonzero(np.isclose(data, -10000.0, rtol=1e-5))}")
+    print(f"n_ndVal: {np.count_nonzero(data == nodata_value)}")
+    print(f"n_ndVal_close: {np.count_nonzero(np.isclose(data, nodata_value, rtol=1e-5))}")
+    near_nodata = data[(data < -9999) & (data > -10001)]
+    print(f"Values close to -10000: {np.unique(near_nodata)}")
+
+    # zero
+    print("ZERO")
+    print(f"n_zero: {np.count_nonzero(data == 0)}")
+    print(f"n_zero_close: {np.count_nonzero(np.isclose(data, 0, atol=1e-8))}")
+
+    #values negative
+    print("NEGATIVE")
+    print(f"n_pos: {np.count_nonzero(data > 0 )}")
+    print(f"n_neg: {np.count_nonzero(data < 0)}")
+    negative_values = data[data < 0]
+    if len(negative_values > 0):
+        print(f" Min negative value: {np.min(negative_values)}")
+        print(f" Max negative value: {np.max(negative_values)}")
+        print(f" Mean of negative values: {np.mean(negative_values)}")
+
+def normalize(data, nodata_value=None):
+    """
+    Normalize data using the Dynamic World paper's method:
+
+    Args:
+        data: numpy array of input values
+        nodata_value: value to be masked/ignored in calculations
+
+    Returns:
+        numpy array of normalized values in range [0,1]
+    """
+    #create a mask if nodata_value is provided
+    if nodata_value is not None:
+        data = np.ma.masked_equal(data, nodata_value)
+
+    # Print diagnostics and Throw error if any negative values  remain after nodata_value is converted to mask
+    negative_values = data[(data < 0) & (~data.mask)]
+    if len(negative_values > 0):
+        counts(data, "data after no value mask", nodata_value)
+        raise ValueError(f"Negative values found in the data after masking: {negative_values}")
+
+    # Log transform
+    # add epsilon to handle zeros
+    log_data = np.log10(data + 1e-10) # masked values '--' do not affect calculation
+    #
+    # Calculate percentiles of log-transformed data
+    if np.ma.is_masked(log_data):
+        pmin = np.percentile(log_data.compressed(), 1)
+        pmax = np.percentile(log_data.compressed(), 99)
+    else:
+        pmin = np.percentile(log_data, 1)
+        pmax = np.percentile(log_data, 99)
+    #
+    # Map percentile[1, 99] to approximately [-2, 2]
+    # shift midpoint to 0, scale range to 1, expand to range
+    transformed_data = 4 * (log_data  - (pmax + pmin) / 2) / (pmax - pmin)
+    # apply sigmoid normalization s.t. OP is (0, 1) and extremes are compressed
+    norm_data = 1/(1+np.exp(-transformed_data)) #sigmoid: 1/(1+e^(-x))
+    #
+    #carry mask forward
+    if nodata_value is not None:
+        norm_data = np.ma.array(norm_data, mask=data.mask)
+    #
+    return {
+        'original': data,
+        'log_transformed': log_data,
+        'scaled': transformed_data,
+        'normalized': norm_data
+    }
+
+def normalize_raster(input_path, output_path, plot):
+    """
+    Normalize a single band raster geotiff file using the Dynamic World normalization method.
+
+    Args:
+        input_path: path to input raster file
+        output_path: path where normalized raster will be saved
+        plot: boolean if histogram are to be made, will be placed at path of output_path
+    """
+
+    with rasterio.open(input_path) as src:
+        success = False
+        data = src.read(1)
+        nodata = src.nodata
+        data_dict = normalize(data, nodata)
+        normalized = data_dict['normalized']
+
+        if plot:
+            directory = os.path.dirname(output_path)
+            raster_name = os.path.splitext(os.path.basename(output_path))[0]
+            plot_transformations(data_dict,
+                                 os.path.join(directory, f'{raster_name}_transformations.png'))
+            plot_histograms(data_dict,
+                            os.path.join(directory, f'{raster_name}_histograms.png'))
+
+        # Prepare the output profile (metadata)
+        profile = src.profile.copy()
+# {'driver': 'GTiff',
+# 'dtype': 'float32',
+# 'nodata': -10000.0,
+# 'width': 2819, 'height': 2417, 'count': 1,
+# 'crs': CRS.from_epsg(32614), 'transform': Affine(0.003, 0.0, 429516.072, 0.0, -0.003, 5524120.8780000005),
+# 'blockysize': 1, 'tiled': False, 'interleave': 'band'}
+
+        dtype_norm = normalized.dtype
+        #print(dtype_norm)
+
+        if dtype_norm != np.float32:
+            warnings.warn(f"The normalized data type is {dtype_norm} Expected to find float32, ensure this is correct for down stream processing")
+
+        profile.update(dtype=dtype_norm)
+
+        # Write the normalized data
+        with rasterio.open(output_path, 'w', **profile) as dst:
+            dst.write(normalized.astype(dtype_norm), 1)
+            print(f"Normalized raster: {output_path}")
+            success = True
+
+        if not success:
+            raise RuntimeError(f"Unsuccessful attempt to produce normalized raster: {output_path}")
+
+def main():
+    """Main function to handle command-line arguments and execute file operations."""
+    print("early exit to test call from common lisp&&&")
+    exit(0)
+    parser = argparse.ArgumentParser(
+        description="File operations utility script",
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "src_file",
+        help="Path to the source file"
+    )
+    parser.add_argument(
+        "dst_file",
+        help="New path and name for the file"
+    )
+    parser.add_argument(
+        "-p", "--plot",
+        action="store_true",
+        help="Enable plots"
+    )
+    args = parser.parse_args()
+
+    normalize_raster(args.src_file, args.dst_file, args.plot)
+    plt.close('all') #ensure matplotlib ends
+
+if __name__ == "__main__":
+    main()
+
+# TODO push to github
---
+++ map-ops.lisp
@@ -0,0 +1,291 @@
+;;;; ===================================  set environment
+                                        ; imports
+(ql:quickload :click)
+
+(defpackage :map-ops
+  (:use
+   :cl
+   :click
+   :py4cl2
+   :cmd
+   :file-finder))
+
+                                        ; enter package
+(in-package :map-ops) ; Also enter this in the REPL!
+
+
+;;;; =================================== merge east-west processing to single standard flight
+
+(defparameter *east-dir* #P"/bulk-1/2023/Pix4D/X2023-07-24_UNMERGED/2023-07-24_eastside/4_index/indices/")
+(defparameter *west-dir* #P"/bulk-1/2023/Pix4D/X2023-07-24_UNMERGED/2023-07-24_re-run_westside/4_index/indices/")
+(defparameter *merge-dir* #P"/bulk-1/2023/Pix4D/X2023-07-24_UNMERGED/2023-07-24_merges/")
+
+(defun F-to-P (F)
+  "Convert one #F filename to #P pathname"
+  (probe-file (path F)))
+
+(defun east-tifs ()
+  "returns a list of #P, being the .tif files below *east-dir*"
+  (mapcar #'F-to-P (finder* :root *east-dir*
+                            :predicates (list
+                                         (extension= "tif")
+                                         (complement (path~ "tiles"))))))
+
+(defun west-tifs ()
+  "returns a list of #P, being the .tif files below *west-dir*"
+  (mapcar #'F-TO-P (finder* :root *west-dir*
+                            :predicates (list
+                                         (extension= "tif")
+                                         (complement (path~ "tiles"))))))
+
+(defun assemble-out-tif (east-tif west-tif)
+  "use east-tif filename to create file path located at *merge-dir*"
+  (merge-pathnames (file-namestring east-tif) *merge-dir*))
+
+(defun out-tifs ()
+  (mapcar #'assemble-out-tif (east-tifs) (west-tifs)))
+
+(defun check-tif-sets (o w e)
+  (flet ((index-only (p) (second (str:split "index_" (pathname-name p)))))
+    ;; all same length
+    (assert (= (length o)
+               (length w)
+               (length e)))
+    ;; all are type #P
+    (assert (every #'pathnamep (append o w e)))
+    ;; all have same indices
+    ;;&&& not working
+    (assert (and (null (set-difference (mapcar #'index-only o)
+                                       (mapcar #'index-only w) :test #'string=))
+                 (null (set-difference (mapcar #'index-only o)
+                                       (mapcar #'index-only e) :test #'string=))))
+    ;; all have all indices, by checking 1, as all are K same
+    (assert (null (set-difference '("sentera_ndre" "blue" "green" "ndvi" "nir" "red" "red_edge")
+                                  (mapcar #'index-only e) :test #'string=)))
+    ;; each file of west and east actually exists
+    (assert (every #'probe-file (append e w)))))
+
+(defun gdal_merge.py (out-tif bottom-tif top-tif)
+  "Usage: gdal_merge.py [-o out_filename] [-of out_format] [-co NAME=VALUE]*
+                     [-ps pixelsize_x pixelsize_y] [-tap] [-separate] [-q] [-v] [-pct]
+                     [-ul_lr ulx uly lrx lry] [-init \"value [value...]\"]
+                     [-n nodata_value] [-a_nodata output_nodata_value]
+                     [-ot datatype] [-createonly] input_files
+                     [--help-general] "
+  (cmd:cmd (format nil "gdal_merge.py -o ~A ~A ~A" (path out-tif) (path bottom-tif) (path top-tif))))
+
+(defun merge-all-indices ()
+  (let ((o (out-tifs))
+        (w (west-tifs))
+        (e (east-tifs)))
+    (check-tif-sets o w e)
+    (mapcar #'gdal_merge.py o w e)))
+
+ ;; (merge-all-indices)
+
+;; ====================================== rename
+
+(defun rename-tifs (&key path replace with (do nil))
+  "Find tifs and fix names"
+  (let* ((pix-root  "/bulk-1/2023/Pix4D/")
+         (files (mapcar #'F-to-P (finder* :root pix-root
+                         :predicates (list (extension= "tif")
+                                           (path~ "4_index")
+                                           (path~ "indices")
+                                           (path~ path)
+                                           (complement (path~ "X2023"))
+                                           (complement (path~ "tiles"))))))
+         ;; get pathname-name
+         (names (mapcar #'pathname-name
+                        files))
+         ;; make new name
+         (new-names (mapcar #'(lambda (n)
+                                (str:replace-first replace with n))
+                            names)))
+    (assert (= 0 (mod (length files) 7)))
+    (format t "n files to rename: ~A" (length files))
+    (print files)
+    (print names)
+    (print new-names)
+
+    ;;rename, controlled by :do t
+    (when do (mapcar #'(lambda (f n)
+                (rename-file f (make-pathname :name n)))
+            files new-names))))
+
+;; normalize naming
+(rename-tifs :path  "10MFlight_SMS_Pix4d_DroboTest"
+             :replace "10MFlight_SMS_Pix4d_DroboTest"
+             :with "SHFlight_SenteraMS_9M")
+
+;; normalize naming
+(rename-tifs :path "teraMS_9M_01_ind"
+             :replace "_01_"
+             :with "_")
+;; fix L typo
+(rename-tifs :path  "SHFLight_SenteraMS_9M"
+             :replace "SHFL"
+             :with "SHFl")
+
+;; all 70 index files to short form
+(rename-tifs :path "SHFlight_SenteraMS_9M_index"
+             :replace "_SHFlight_SenteraMS_9M_"
+             :with "_")
+
+;; ====================================== copy all tifs
+
+;; gather a copy of all tifs to a single dir
+(defun collect ()
+  (mapcar #'(lambda (f)
+              (uiop:copy-file f (make-pathname :defaults f
+                                               :directory (pathname-directory #P"/bulk-1/rasters/"))))
+          (mapcar #'F-to-P (finder* :root "/bulk-1/2023/Pix4D/"
+                                    :predicates (list (extension= "tif")
+                                                      (path~ "4_index")
+                                                      (path~ "indices")
+                                                      (complement (path~ "X2023"))
+                                                      (complement (path~ "tiles")))))))
+;; (collect)
+
+;; ====================================== clip to AOI buffered
+
+(defun gdalwarp-clip (gpkg-shape srs-tif dst-tif)
+  "clip srs-tif to the shape defined in gpkg-shape, saving as dst-tif"
+  ($cmd (format nil "gdalwarp -of GTiff -cutline ~A -crop_to_cutline ~A ~A" gpkg-shape srs-tif dst-tif)))
+
+(defun clip-to-buffer (srs-tif)
+  "Apply AOI buffered shape, to srs-tif and make a new name appending _buffer"
+  (let* ((shape #P"/home/user/qgis/AOI-buffered.gpkg")
+         (postfix "_buffer")
+         (new-name (make-pathname :defaults srs-tif
+                                  :name (concatenate 'string
+                                                     (pathname-name srs-tif)
+                                                     postfix))))
+    (gdalwarp-clip shape srs-tif new-name)))
+
+(defun tifs-clipped-to-buffer ()
+  "Gathers all full size .tif and clips to buffered aoi, tests for 70"
+  (let ((tifs (mapcar #'F-to-P (finder* :root "/bulk-1/rasters/"
+                                         :predicates (list (extension= "tif")
+                                                           (complement (name~ "_buffer")))))))
+    (assert (= 70 (length tifs)) () "Expected 70 found ~A" (length tifs))
+    (mapcar #'clip-to-buffer tifs)))
+
+;;(tifs-clipped-to-buffer)
+
+
+;; ====================================== clip to test buffered,  repeat clip for test area!
+(defun clip-to-test-buffer (srs-tif)
+  "Apply test buffered shape, to srs-tif and make a new name appending _test-buffer"
+  (let* ((shape #P"/home/user/qgis/test-AOI-buffered.gpkg")
+         (postfix "_test-buffer")
+         (new-name (make-pathname :defaults srs-tif
+                                  :name (concatenate 'string
+                                                     (pathname-name srs-tif)
+                                                     postfix))))
+    (gdalwarp-clip shape srs-tif new-name)))
+
+(defun tifs-clipped-to-test-buffer ()
+  "Gathers all full size .tif and clips to buffered test area, tests for 70"
+  (let ((tifs (mapcar #'F-to-P (finder* :root "/bulk-1/rasters/"
+                                         :predicates (list (extension= "tif")
+                                                           (complement (name~ "_test-buffer"))
+                                                           (complement (name~ "_buffer")))))))
+    (assert (= 70 (length tifs)) () "Expected 70 found ~A" (length tifs))
+    (mapcar #'clip-to-test-buffer tifs)))
+
+;;(tifs-clipped-to-test-buffer)
+
+;; ====================================== fill pixels with no data
+
+(defun gdal_fillnodata (srs-tif dst-tif)
+  "heal nodata pixels in srs-tif, saving as dst-tif"
+  ($cmd (format nil "gdal_fillnodata.py -md 100 -si 11 -of GTiff ~A ~A" srs-tif dst-tif)))
+
+(defun fillnodata (srs-tif)
+  "fill pixels in srs-tif, create a new name, appending _filled"
+  (let* ((postfix "_filled")
+         (new-name (make-pathname :defaults srs-tif
+                                  ::name (concatenate 'string
+                                                      (pathname-name srs-tif)
+                                                      postfix))))
+    (gdal_fillnodata srs-tif new-name)))
+
+(defun tifs-filled ()
+  "gather all clipped tiffs and fills nodata pixels, tests for 70+70 "
+  (let* ((test-tifs (mapcar #'F-to-P (finder* :root "/bulk-1/rasters/"
+                                              :predicates (list (extension= "tif")
+                                                                (name~ "_test-buffer")
+                                                                (complement (name~ "_filled"))))))
+         (full-tifs (mapcar #'F-to-P (finder* :root "/bulk-1/rasters/"
+                                              :predicates (list (extension= "tif")
+                                                                (name~ "_buffer")
+                                                                (complement (name~ "_filled"))))))
+         (tifs (append test-tifs full-tifs)))
+    (assert (= 140 (length tifs)) () "Expected 140, found ~A" (length tifs))
+    (mapcar #'fillnodata tifs)))
+
+;;(tifs-filled)
+
+;; ====================================== pixel alignment and resampling
+(defun gdalwarp-tap (src-tif dst-tif)
+  "resample to 3mm pixels and target aligned pixels"
+  ($cmd (format nil "gdalwarp -tr 0.003 0.003 -r average -tap -of GTiff -overwrite ~A ~A" src-tif dst-tif)))
+
+(defun resample-tap (srs-tif)
+  "align pixels in srs-tif, create a new name, appending _aligned"
+  (let* ((postfix "_aligned")
+         (new-name (make-pathname :defaults srs-tif
+                                  ::name (concatenate 'string
+                                                      (pathname-name srs-tif)
+                                                      postfix))))
+    (gdalwarp-tap srs-tif new-name)))
+
+(defun tifs-aligned ()
+  "gather all filled tiffs and aligns pixels, tests for 70+70 "
+  (let* ((test-tifs (mapcar #'F-to-P (finder* :root "/bulk-1/rasters/"
+                                              :predicates (list (extension= "tif")
+                                                                (name~ "_test-buffer_filled")
+                                                                (complement (name~ "_aligned"))))))
+         (full-tifs (mapcar #'F-to-P (finder* :root "/bulk-1/rasters/"
+                                              :predicates (list (extension= "tif")
+                                                                (name~ "_buffer_filled")
+                                                                (complement (name~ "_aligned"))))))
+         (tifs (append test-tifs full-tifs)))
+    (assert (= 140 (length tifs)) () "Expected 140, found ~A" (length tifs))
+    (mapcar #'resample-tap tifs)
+    ))
+
+;;(tifs-aligned)
+
+;; ====================================== normalize
+
+(ql:quickload :py4cl2)
+(use-package :py4cl2)
+;; (py4cl2:initialize) ; python3 ret ret ret
+;; (pyversion-info)
+
+(defpymodule "os" nil :lisp-package "PYOS")
+(py-cd "/home/user/")
+(pyos:getcwd)
+
+(defpymodule "numpy" nil :lisp-package "NP")
+(np:floor 4.20)
+
+;;#|;; vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv end of compiled code
+(error "Beyond here be monsters") ;; ensure #| is active to exclude construction from compilation
+
+;; ====================================== build
+
+(defpymodule "rasterio" nil :lisp-package "RIO")
+
+;; ====================================== scratch
+np.floor(4.2)
+;; ====================================== reference
+
+;;;; vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv later
+
+
+;; gaussian pyramid
+;; clip to AOI
+;; export to gpkg
